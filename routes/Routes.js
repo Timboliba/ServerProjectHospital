8e72f31748e78e22db2807de5b16d95e8cba23bb
Hamadi <hamaditimboliba54@gmail.com>
2024-02-22 02:35:20 +0100

    ajout de la route pour la suppression des planification du docteur v 1.1

--- routes/Routes.js
+++ routes/Routes.js
@@ -365,10 +365,10 @@ App.get('/api/Planification/:id_docteur', (req, res, next) => {
     .catch(error => res.status(400).json({ error: error.message }));
 });
 
-App.delete('/api/Planification/:id',(res,req)=>{
-  Planification.deleteOne({_id:req.params.id})
-  .then(result=>res.status(200).json(result))
-  .catch(error=>res.status(400).json({error:error.message}))
+App.delete('/api/Planification/:id',(req,res)=>{
+  Planification.deleteOne({ _id: req.params.id })
+  .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+  .catch(error => res.status(400).json({ error }));
 })
 
 module.exports =App;
